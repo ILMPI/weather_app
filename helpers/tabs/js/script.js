b170031fe333c0c45ef1b71127506ab01cf33c90
Yevhenii <Yevhenii.Boliekhovskyi@promx.net>
2025-02-11 13:44:17 +0200

    fix: function tabs

--- helpers/tabs/js/script.js
+++ helpers/tabs/js/script.js
@@ -22,27 +22,34 @@ tabsNavAll.forEach(function (item) {
 */
 
 function tabs(tabsBlockSelector) {
-	const tabsNavAll = document.querySelectorAll(tabsBlockSelector + " [data-tab]");
-	const tabsContentAll = document.querySelectorAll(tabsBlockSelector + " [data-tab-content]");
+	const tabsNavAll = document.querySelectorAll(`${tabsBlockSelector} [data-tab]`);
+	const tabsContentAll = document.querySelectorAll(`${tabsBlockSelector} [data-tab-content]`);
 
-	tabsNavAll.forEach(function (item) {
-		item.addEventListener("click", function (event) {
-			if (item.classList.contains("active")) return;
+	tabsNavAll.forEach(tabNav => {
+		tabNav.addEventListener('click', event => {
+			// Check if the tab is already active
+			if (tabNav.classList.contains('active')) return;
+			// Remove the active class from all tabs and content
 
-			tabsNavAll.forEach((i) => {
-				i.classList.remove("active");
-			});
+			tabsNavAll.forEach(nav => nav.classList.remove('active'));
+			tabsContentAll.forEach(content => content.classList.remove('active'));
 
-			item.classList.add("active");
+			// Add the active class to the current tab
+			tabNav.classList.add('active');
 
-			tabsContentAll.forEach((i) => {
-				i.classList.remove("active");
-			});
+			// Find the corresponding content
+			const targetContentId = event.currentTarget.dataset.tab;
+			const targetContent = document.querySelector(`#${targetContentId}`);
 
-			document.querySelector("#" + event.target.dataset.tab).classList.add("active");
+			// Check if the content exists
+			if (targetContent) {
+				targetContent.classList.add('active');
+			} else {
+				console.error(`Content with id="${targetContentId}" not found!`);
+			}
 		});
 	});
 }
 
-tabs(".tabsBlock1");
-tabs(".tabsBlock2");
+tabs('.tabsBlock1');
+tabs('.tabsBlock2');
